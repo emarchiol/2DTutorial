ad949df5340a96de8070bc6c9e86c87e81727b56
emarchiol <emilianomarchiol@gmail.com>
2015-07-27 13:09:39 -0300

    just backup

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed = 50, jumpVelocity = 150;
+	bool isGround = false;
+	Transform playerTransf;
+	Rigidbody2D playerBody;
+
+	void Start () {
+		playerBody = this.GetComponent<Rigidbody2D>();
+		playerTransf = this.GetComponent<Transform>();
+	}
+
+	void FixedUpdate () {
+
+		//Move with keyboard and joystick
+		playerBody.AddForce((Vector2.right * speed) * Input.GetAxis("Horizontal"));
+	}
+
+	public void Move(float horizontalInput){
+
+	}
+}
